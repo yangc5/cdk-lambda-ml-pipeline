c34227c3642473d9b67240c381faec01dfad0aae
chen yang <awschen@amazon.com>
2022-08-17 16:55:12 -0700

    image codebuild now working

--- cdk_ml_pipeline/cdk_ml_pipeline_stack.py
+++ cdk_ml_pipeline/cdk_ml_pipeline_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     Stack,
     aws_lambda,
     aws_iam,
+    aws_ecr as ecr,
     aws_s3,
     aws_s3_deployment as s3deploy,
     aws_stepfunctions as sfn,
@@ -17,9 +18,15 @@ class CdkMlPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-# Grant training Lambda read write permissions for S3 bucket where data and model artifacts are stored
-        s3_statement=aws_iam.PolicyStatement(
+# Read write permissions for S3 bucket where data and model artifacts are stored
+        model_artifacts_bucket_read_write_statement=aws_iam.PolicyStatement(
             actions=["s3:List*","s3:Get*","s3:PutObject","s3:PutObjectAcl","s3:DeleteObject"],
+            resources=["arn:aws:s3:::cdk-ml-pipeline-iris", "arn:aws:s3:::cdk-ml-pipeline-iris/*"]
+        )
+
+# Read only permission for data and model artifacts bucket
+        model_artifacts_bucket_read_only_statement=aws_iam.PolicyStatement(
+            actions=["s3:List*","s3:Get*"],
             resources=["arn:aws:s3:::cdk-ml-pipeline-iris", "arn:aws:s3:::cdk-ml-pipeline-iris/*"]
         )
 
@@ -35,4 +42,117 @@ class CdkMlPipelineStack(Stack):
         )
 
 # Attach S3 policy to Training Lambda execution role
-        training_lambda.role.add_to_policy(s3_statement)
+        training_lambda.role.add_to_policy(model_artifacts_bucket_read_write_statement)
+
+# S3 bucket for inference results during inferencing process
+        inference_results_bucket = aws_s3.Bucket(self, "inference-results-bucket", bucket_name="cdk-ml-pipeline-iris-model-inference-results-bucket",versioned=True)
+
+# Inference Lambda execution role
+        inference_lambda_execution_role = aws_iam.Role(self,
+            'inference-lambda-execution-role',
+            assumed_by=aws_iam.CompositePrincipal(
+                aws_iam.ServicePrincipal('lambda.amazonaws.com')
+            ),
+            managed_policies=[
+                aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+            ])
+
+# Inference Lambda image ECR
+        inference_ecr= ecr.Repository(self, "inference-ecr", repository_name="inference-ecr")
+
+# S3 bucket for CodeBuild artifacts including buildspec.yml
+        codebuild_artifacts_bucket = aws_s3.Bucket(self, "codebuild-bucket", bucket_name="cdk-ml-pipeline-codebuild-artifacts-bucket",versioned=True)
+
+# inference_image_codebuild project
+        inference_image_codebuild_bucket_deployment = s3deploy.BucketDeployment(self, "inference-image-codebuild-artifacts-bucket-deployment",
+            sources=[s3deploy.Source.asset("inference_image_codebuild"), s3deploy.Source.asset("inference_lambda")],
+            destination_bucket=codebuild_artifacts_bucket,
+            destination_key_prefix="inference-image-codebuild"
+        )
+
+        inference_image_codebuild_s3_source = codebuild.Source.s3(
+            bucket=codebuild_artifacts_bucket,
+            path="inference-image-codebuild/"
+        )
+
+        inference_image_codebuild_project = codebuild.Project(self, "inference-image-codebuild-project",
+            project_name="inference-image-codebuild-project",
+            source=inference_image_codebuild_s3_source,
+            environment=codebuild.BuildEnvironment(
+                privileged=True
+            ),
+            environment_variables={
+                "AWS_ACCOUNT_ID": codebuild.BuildEnvironmentVariable(
+                    value=cdk.Aws.ACCOUNT_ID
+                ),
+                "IMAGE_REPO_NAME": codebuild.BuildEnvironmentVariable(
+                    value=inference_ecr.repository_name
+                )
+            }
+        )
+
+        inference_image_codebuild_project.role.add_to_policy(model_artifacts_bucket_read_only_statement)
+        codebuild_artifacts_bucket.grant_read(inference_image_codebuild_project.role)
+        inference_ecr.grant_pull_push(inference_image_codebuild_project.role)
+
+# create_or_update_inference_lambda_codebuild project
+        create_or_update_inference_lambda_codebuild_bucket_deployment = s3deploy.BucketDeployment(self, "create-or-update-inference-lambda-codebuild-artifacts-bucket-deployment",
+            sources=[s3deploy.Source.asset("create_or_update_inference_lambda_codebuild")],
+            destination_bucket=codebuild_artifacts_bucket,
+            destination_key_prefix="create-or-update-inference-image-codebuild"
+        )
+
+        create_or_update_inference_lambda_codebuild_s3_source = codebuild.Source.s3(
+            bucket=codebuild_artifacts_bucket,
+            path="create-or-update-inference-image-codebuild/"
+        )
+
+        create_or_update_inference_lambda_project = codebuild.Project(self, "create-or-update-inference-lambda-project",
+            project_name="create-or-update-inference-lambda-project",
+            source=create_or_update_inference_lambda_codebuild_s3_source,
+            environment_variables={
+                "AWS_ACCOUNT_ID": codebuild.BuildEnvironmentVariable(
+                    value=cdk.Aws.ACCOUNT_ID
+                ),
+                "IMAGE_REPO_NAME": codebuild.BuildEnvironmentVariable(
+                    value=inference_ecr.repository_name
+                ),
+                "INFERENCE_IMAGE_URI":codebuild.BuildEnvironmentVariable(
+                    value=cdk.Aws.ACCOUNT_ID+".dkr.ecr."+cdk.Aws.REGION+".amazonaws.com/"+inference_ecr.repository_name
+                ),
+                "INFERENCE_RESULTS_BUCKET":codebuild.BuildEnvironmentVariable(
+                    value=inference_results_bucket.bucket_name
+                ),
+                "INFERENCE_LAMBDA_EXECUTION_ROLE_ARN":codebuild.BuildEnvironmentVariable(
+                    value=inference_lambda_execution_role.role_arn
+                )
+            }
+        )
+
+        codebuild_artifacts_bucket.grant_read(create_or_update_inference_lambda_project.role)
+
+        create_or_update_inference_lambda_project.role.attach_inline_policy(aws_iam.Policy(self, "inference-lambda-update-policy",
+            statements=[
+                aws_iam.PolicyStatement(
+                    actions=["lambda:GetFunction", "lambda:CreateFunction","lambda:UpdateFunctionCode"],
+                    resources=["arn:aws:lambda:"+cdk.Aws.REGION+":"+cdk.Aws.ACCOUNT_ID+":function:training-lambda"]
+                ),
+                aws_iam.PolicyStatement(
+                    actions=["iam:GetRole", "iam:PassRole"],
+                    resources=[inference_lambda_execution_role.role_arn]
+                )
+            ]
+        ))
+
+        create_or_update_inference_lambda_project.role.attach_inline_policy(aws_iam.Policy(self, "inference-lambda-update-ecr-access-policy",
+            statements=[
+                aws_iam.PolicyStatement(
+                    actions=["ecr:*"],
+                    resources=[inference_ecr.repository_arn]
+                ),
+                aws_iam.PolicyStatement(
+                    actions=["ecr:GetAuthorizationToken"],
+                    resources=["*"]
+                )
+            ]
+        ))
---
+++ inference_lambda/inference.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import boto3
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+import pickle
+import datetime
+
+def handler(event, context):
+
+    s3 = boto3.resource('s3')
+    data_and_model_bucket="cdk-ml-pipeline-iris"
+    key="data/Iris.csv"
+    s3.Bucket(data_and_model_bucket).download_file(key, "/tmp/Iris.csv")
+
+    iris = pd.read_csv("/tmp/Iris.csv")
+
+    train, test = train_test_split(iris, test_size = 0.3)
+    # in this our main data is split into train and test
+    # the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
+
+    train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]# taking the training data features
+    train_y=train.Species# output of our training data
+    test_X= test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] # taking test data features
+    test_y =test.Species   #output value of test data
+
+    # load pre trained model
+    # model_key="models/latest/finalized_model.sav"
+    # s3.Bucket(data_and_model_bucket).download_file(model_key, "/tmp/finalized_model.sav")
+
+    model=pickle.load(open("finalized_model.sav", "rb"))
+    prediction=model.predict(test_X)
+    print('The accuracy of the KNN is',metrics.accuracy_score(prediction,test_y))
+
+    prediction_csv = pd.DataFrame(predictions, columns=['predictions']).to_csv('/tmp/prediction.csv')
+
+    t = datetime.datetime.now()
+    path="inference-results/"+t.strftime('%m-%d-%Y %H-%M-%S')+"/prediction.csv"
+
+    s3.Bucket(os.environ['INFERENCE_RESULTS_BUCKET']).upload_file('/tmp/prediction.csv', path)
